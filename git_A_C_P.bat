7364499bae390acdca695046261d3668a34c605a
ComputerUncle <mailtojovichua@gmail.com>
2025-11-08 20:55:38 +0800

    Updated to commit specific file

--- git_A_C_P.bat
+++ git_A_C_P.bat
@@ -1,8 +1,9 @@
 @echo off
 echo git ALL COMMIT PUSH
+set /p file=File to git add: 
 set /p msg=Please enter your commit message: 
 
-git add .
+git add %file%
 git commit -m "%msg%"
 git push
 pause
